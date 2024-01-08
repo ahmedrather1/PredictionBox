deda6363f98088aa15c62f4a3e6aaa8bffd452f3
Ahmed Rather <ahmedrather.s@gmail.com>
2024-01-08 14:50:04 -0500

    adding file upload

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import KnnPage from "./KnnPage";
+import CustomKnnPage from "./CustomKnnPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<KnnPage />} />
+        <Route path="/" element={<CustomKnnPage />} />
       </Routes>
     </Router>
   );
