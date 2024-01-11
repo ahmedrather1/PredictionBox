fb7c8b87ca2cf5b2fd85b9619904e89b89b6b362
Ahmed Rather <ahmedrather.s@gmail.com>
2024-01-11 12:24:38 -0500

    removed all styling

--- app/src/pages/CustomKnnPage.js
+++ app/src/pages/CustomKnnPage.js
@@ -7,52 +7,6 @@ import Papa from "papaparse";
 import ChartComponent from "../components/common/ChartComponent";
 import CustomForm from "../components/common/CustomForm";
 
-// Styled components
-const StyledHeader = styled(Navbar)`
-  background-color: #d7bde2;
-  font-family: Georgia, serif;
-  padding: 15px 0;
-`;
-
-const MainContent = styled.div`
-  padding: 20px;
-`;
-
-const HeaderContainer = styled(Container)`
-  max-width: 100%; // Allow full width
-`;
-
-const FlexContainer = styled.div`
-  display: flex;
-  justify-content: flex-start; // Align items to the start (left)
-  align-items: center; // Center the content vertically
-  padding: 20px;
-`;
-
-const GraphContainer = styled.div`
-  flex: 0 0 40%; // The GraphComponent will take up 25% of the container width and will not grow or shrink
-  // You can adjust the flex-basis percentage to control the width more precisely
-`;
-
-// Header Component
-function Header() {
-  return (
-    <StyledHeader bg="purple" expand="lg">
-      <HeaderContainer>
-        <Navbar.Brand href="#home">Mess With ML</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#login">Login</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </HeaderContainer>
-    </StyledHeader>
-  );
-}
-
 // CustomKnnPage Component
 function CustomKnnPage() {
   const [columns, setColumns] = useState(null);
@@ -300,7 +254,7 @@ function CustomKnnPage() {
 
     if (samplePrediction && originalData) {
       return (
-        <GraphContainer>
+        <>
           <ChartComponent
             samplePrediction={samplePrediction}
             originalData={originalData}
@@ -324,7 +278,7 @@ function CustomKnnPage() {
           {customIndividualPrediction && (
             <h3>Custom Individual Prediction {customIndividualPrediction}</h3>
           )}
-        </GraphContainer>
+        </>
       );
     }
 
@@ -342,11 +296,8 @@ function CustomKnnPage() {
 
   return (
     <div>
-      <Header />
-      <MainContent>
-        <h3>K Nearest Neighbors</h3>
-        <FlexContainer>{renderContent()}</FlexContainer>
-      </MainContent>
+      <h3>K Nearest Neighbors</h3>
+      <>{renderContent()}</>
     </div>
   );
 }
