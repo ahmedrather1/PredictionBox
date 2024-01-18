929d155cfd8113ed5e24ba73529d539735fea407
Ahmed Rather <ahmedrather.s@gmail.com>
2024-01-17 19:21:01 -0500

    updating allowed hosts

--- engine/engineapi/engineapi/settings.py
+++ engine/engineapi/engineapi/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['prediction-box-engine-env.eba-cqvymbxb.us-west-2.elasticbeanstalk.com', '127.0.0.1']
+ALLOWED_HOSTS = ['prediction-box-engine-env.eba-cqvymbxb.us-west-2.elasticbeanstalk.com', '127.0.0.1', '*.elasticbeanstalk.com']
 
 
 # Application definition
