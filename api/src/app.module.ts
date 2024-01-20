1c1661276a7f61fb0b1bfc91181b4d39fec60319
Ahmed Rather <ahmedrather.s@gmail.com>
2024-01-20 14:59:27 -0500

    environment variables and form updates for api

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,9 +5,16 @@ import { ApiGatewayModule } from './api-gateway/api-gateway.module';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { DemoDataModule } from './demo-data/demo-data.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ApiGatewayModule, UserModule, DatabaseModule, DemoDataModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ApiGatewayModule,
+    UserModule,
+    DatabaseModule,
+    DemoDataModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
