448160d85e65b77bcc16915fa0b0407b1ddde272
Ahmed Rather <ahmedrather.s@gmail.com>
2024-01-26 18:13:18 -0500

    boilerplate

--- api/src/api-gateway/api-gateway.module.ts
+++ api/src/api-gateway/api-gateway.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { KnnGatewayController } from './knn-gateway/knn-gateway.controller';
 import { HttpModule, HttpService } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
+import { SlrGatewayController } from './slr-gateway/slr-gateway.controller';
 
 @Module({
   imports: [HttpModule, ConfigModule],
-  controllers: [KnnGatewayController],
+  controllers: [KnnGatewayController, SlrGatewayController],
 })
 export class ApiGatewayModule {}
