ac794c1480a06a7a4a07e67b4411df4a3542fcef
Ahmed Rather <ahmedrather.s@gmail.com>
2024-02-19 16:24:48 -0500

    updating partial regressions to return regression line for partial regressions

--- engine/engineapi/mlr/predictions/PartialRegressions.py
+++ engine/engineapi/mlr/predictions/PartialRegressions.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from slr.models.slrModel import sampleSlrModel
 from statsmodels.api import OLS, add_constant
+import numpy as np
+
 import ast
 
 def partialRegressions(file, predictorsString, response):
@@ -15,10 +18,10 @@ def partialRegressions(file, predictorsString, response):
     if response not in data.columns:
             raise KeyError("Invalid response")
     if len(predictors) == 0:
-            raise KeyError("Invalid predictors array")
+            raise KeyError("Invalid predictors array: no predictors!")
     for predictor in predictors:
         if predictor not in data.columns:
-            raise KeyError("Invalid predictors array")
+            raise KeyError("Invalid predictors array: " + predictor + " not in your dataset")
     
     X = data[predictors]
     y = data[response]
@@ -32,10 +35,27 @@ def partialRegressions(file, predictorsString, response):
         
         predictorModel = OLS(X[predictor], add_constant(XOtherPredictors)).fit()
         predictorResiduals = X[predictor] - predictorModel.predict(add_constant(XOtherPredictors))
+        predictorResiduals2d = predictorResiduals.values.reshape(-1, 1)
+
+        xRaw = predictorResiduals.tolist()
+        yRaw = yResiduals.tolist()
+
+        slrModel = sampleSlrModel(predictorResiduals2d, yResiduals)
+        minValue = X[predictor].min() 
+        maxValue = X[predictor].max()  
+        xRange = np.linspace(minValue, maxValue, 500).reshape(-1, 1)  
+        yPred = slrModel.predict(xRange)
         
         partialRegressionsData[predictor] = {
-            'x': predictorResiduals.tolist(),
-            'y': yResiduals.tolist()
+            'raw': {            
+                'x': xRaw,
+                'y': yRaw
+            },
+            'regressed': {
+                'x': xRange.flatten().tolist(),
+                'y': yPred.tolist()
+            }
+
         }
     
     return partialRegressionsData
