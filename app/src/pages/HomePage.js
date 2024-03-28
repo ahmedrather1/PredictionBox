dfab8f960c6189b3904cd8f499907165aaf96ee8
Ahmed Rather <ahmedrather.s@gmail.com>
2024-03-28 12:33:35 -0400

    bug fix

--- app/src/pages/HomePage.js
+++ app/src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useEffect} from "react";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import Header from "../components/common/Header";
